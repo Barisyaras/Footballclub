c802234cb7e4c08125cbeaed92fb09b746bdcfab
barisyaras <ismayaras@hotmail.com>
2022-07-12 01:28:54 +0300

    OK

--- Footballclub/SportSimulation.cs
+++ Footballclub/SportSimulation.cs
@@ -9,10 +9,7 @@
         {
             var result = ((double)team1.Value) / (team1.Value + team2.Value) * 100;
             return(int)Math.Round(result);
-
-
         }
-
 
         public void SimulateMatch(Team team1,Team team2) 
         {
@@ -20,16 +17,16 @@
             var posability = rnd.Next(1, 100);
 
             SportSimulation simulation = new SportSimulation();
-            int edgeValue = simulation.Simulate(team1, team2);
+            var edgeValue = simulation.Simulate(team1, team2);
             if (posability < edgeValue)
             {
                 Console.WriteLine(team1.Name + " Kazandı");
-                Console.WriteLine(edgeValue);
             }
 
             else if (posability == edgeValue)
             {
                 Console.WriteLine("Beraberlik");
+
             }
 
             else
